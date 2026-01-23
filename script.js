80f26d2b836234f4bb9ea8eda73b5231a455115a
1dayzy1 <dayzy173@gmail.com>
2026-01-23 15:07:57 +0300

    init

---
+++ script.js
@@ -0,0 +1,26 @@
+const arr_name = ["Dima", 'Artem', "Nikita"];
+
+// const arr = new Array();
+
+console.log(arr_name);
+
+arr_name.unshift("Vlad");
+
+console.log(arr_name);
+
+arr_name.shift();
+
+
+console.log(arr_name);
+
+arr_name.push("Vlad");
+
+console.log(arr_name);
+
+arr_name.pop();
+
+console.log(arr_name);
+
+arr_name.splice(1, 1, "Vlad");
+
+console.log(arr_name);
